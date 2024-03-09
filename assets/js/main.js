ed94956233ef22c32646f23effb7329d6a65dee9
Pedro Magno <pedromagnopro@gmail.com>
2024-03-08 21:49:16 -0300

    Projeto na versão 1.0.0

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+let state = {
+    view:{
+        cartoon: document.getElementById('cartoon'),
+        language: document.getElementById('language'),
+    },
+    values:{
+        languages:['csharp','golang','nodejs','js'],
+    }
+}
+
+function sorteiaNumero(){
+    // quantidade de imagens que tenho no repositorio cartoons
+    const quantidadeImagensCartoon = 33
+    let numeroSorteado = Math.floor(Math.random()*quantidadeImagensCartoon);
+    return numeroSorteado;
+}
+
+
+function sorteiaLinguagem(){
+    let numeroAleatorio = Math.floor(Math.random()*state.values.languages.length);
+    let linguagemSelecionada = state.values.languages[numeroAleatorio];
+    return linguagemSelecionada;
+}
+
+const iniciar = () =>{
+    setInterval(()=>{
+        let num = sorteiaNumero();
+        let language = sorteiaLinguagem();
+        state.view.cartoon.src=`/assets/img/cartoons/${num}.png`
+        state.view.language.src=`/assets/img/linguagens/${language}.png`        
+    },2000);
+}
+
+
+iniciar()
